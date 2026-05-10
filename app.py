71eae7d21162a3ef9c2d9914381fee5822b6c796
nabiyakathoon2009-cmyk <nabiyakathoon2009@gmail.com>
2026-05-10 22:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
 
     return """
     <h1>🚀 Training Trains</h1>
 
-    <h2>Internship Portal</h2>
+    <h2>Website Working Successfully 😎</h2>
 
-    <p>Website Working Successfully 😎</p>
+    <p>Internship Portal Live</p>
     """
 
-
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=10000)
